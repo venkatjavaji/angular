1711603e328ac1dbc0b8b2d804f661759310ef67
venkat <javaji.venkat@gmail.com>
2020-08-12 16:37:17 +0530

    upload project to guthub

---
+++ angular5/ng-pwa/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  constructor(private swUpdate:SwUpdate){}
+
+  ngOnInit(): void {
+    
+    if(this.swUpdate.isEnabled){
+      this.swUpdate.available.subscribe(
+        ()=>{
+            if(confirm("New version is available. Load New Version?"))
+            window.location.reload();
+        }
+      );
+    }
+  }
+  title = 'Progressive web app';
+}
